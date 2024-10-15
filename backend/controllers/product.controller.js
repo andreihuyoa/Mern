c70305acf390cc9fbc0f964f9e4d0e0b4a112522
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2024-10-15 20:40:10 +0800

    Update product.controller.js

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -31,7 +31,7 @@ export const createProducts = async (req, res) => {
 
 export const updateProduct = async (req, res) => {
   const { id } = req.params;
- 
+
   const product = req.body;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -50,11 +50,15 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid Product Id" });
+  }
+
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product Deleted" });
   } catch (error) {
     console.log("Error in deleting product");
-    res.status(404).json({ success: false, message: "Product Not Found" });
+    res.status(500).json({ success: false, message: "Server Error" });
   }
 };
