081d73c15a9e22385402384c02f0483787614690
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2024-10-16 23:34:20 +0800

    updated product.js
    also vite

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,9 +1,22 @@
 import { create } from "zustand";
 
+/*
 // const [state, setState] = useState([]); // eto local state
-
-//eto global which means we can use in any components sa app
+* eto global which means we can use in #any! components sa app
+*/
 export const useProductStore = create((set) => ({
   products: [],
   setProducts: (products) => set({ products }),
+  createProduct: async (newProduct) => {
+    if (!newProduct.name || !newProduct.price || !newProduct.image) {
+      return { success: false, message: "Please fill in all fields." };
+    }
+    const res = await fetch("/api/products", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newProduct),
+    });
+    const data = await res.JSON();
+    set((state) => ({ products: [...state.products, data.data] }));
+  },
 }));
