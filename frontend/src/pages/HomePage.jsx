8d7ccadd166b3e8adba918e307f490f64ae02417
Andrei Huyo-a <91776408+andreihuyoa@users.noreply.github.com>
2024-10-19 16:40:23 +0800

    added delete product

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
 
 import { Container, Text, VStack, SimpleGrid, Box } from "@chakra-ui/react";
 
+import ProductCard from "../components/ProductCard";
+
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products);
   return (
     <Container maxW={Container.xl} py={12}>
       <VStack spacing={8}>
@@ -17,17 +25,24 @@ const HomePage = () => {
           Current Products 🚀
         </Text>
 
-        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w={"full"}></SimpleGrid>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} w={"full"}>
+          {products.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </SimpleGrid>
 
-        <Text fontSize={"xl"} textAlign={"center"} fontWeight={"bold"} color={"gray.500"}>
-          No products found🥲
-          {/* This link tag should be coming from react router instead of chakra ui */}
-          <Link to={"/create"}>
-            <Text as={"span"} color={"blue.500"} _hover={{ textDecoration: "underline" }}>
-              Create a product
-            </Text>
-          </Link>
-        </Text>
+        {/* This is the condition */}
+        {products.length === 0 && (
+          <Text fontSize={"xl"} textAlign={"center"} fontWeight={"bold"} color={"gray.500"}>
+            No products found🥲
+            {/* This link tag should be coming from react router instead of chakra ui */}
+            <Link to={"/create"}>
+              <Text as={"span"} color={"blue.500"} _hover={{ textDecoration: "underline" }}>
+                Create a product
+              </Text>
+            </Link>
+          </Text>
+        )}
       </VStack>
     </Container>
   );
